f0c765c81e0787895cd023704d490f7ddae53b5a
Habib Gemmechu <95415986+habib-gm@users.noreply.github.com>
2023-10-21 14:22:18 +0300

    Update Alternating Subsequence.py -- added another approach
    another approach added using pointer

--- Alternating Subsequence.py
+++ Alternating Subsequence.py
@@ -18,15 +18,48 @@ def alternatingSubsequence(n: int, nums: list):
      result +=  max_pos 
             
      print(result)
+
+# BELOW IS APPROACH 2 USING TWO POINTER
+# UNCOMMENT IT TO RUN
+
+# if __name__ == "__main__":
+#     t = int(input())
+#     for i in range(t):
+#         n = int(input())
+#         nums = list(map(int, input().split()))
+        
+#         alternatingSubsequence(n, nums)
+    
+
+# def alternatingSubsequence(n: int, nums: list):
+#      l, r = 0, 0
+#      result = 0
+     
+#      while(l < n and r < n):
+#          if(nums[r] < 0):
+#              if(nums[l] > 0):
+#                  result += nums[l]
+#                  l = r
+#              elif(nums[r] > nums[l]):
+#                  l = r
+#          else:
+#              if(nums[l] < 0):
+#                  result += nums[l]
+#                  l = r
+#              elif(nums[r] > nums[l]):
+#                  l = r
+#          r += 1
+    
+#      result += nums[l]
+            
+#      print(result)
              
      
 
-if __name__ == "__main__":
-    t = int(input())
-    for i in range(t):
-        n = int(input())
-        nums = list(map(int, input().split()))
+# if __name__ == "__main__":
+#     t = int(input())
+#     for i in range(t):
+#         n = int(input())
+#         nums = list(map(int, input().split()))
         
-        alternatingSubsequence(n, nums)
-    
-    
+#         alternatingSubsequence(n, nums)
