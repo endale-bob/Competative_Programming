451ef97a3f58614f10d4051204bc92f47c0cb450
Habib Gemmechu <95415986+habib-gm@users.noreply.github.com>
2023-10-21 09:59:53 +0300

    Create dividePlayers.py
    Leetcode #SOLVED
    https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/

---
+++ dividePlayers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        total = skill[0] + skill[-1]
+        chem = 0
+
+        for i in range(len(skill)//2):
+            if(total != skill[i] + skill[len(skill) - i - 1]):
+                return -1
+            else:
+                chem += skill[i] * skill[len(skill) - i - 1]
+        
+        return chem
