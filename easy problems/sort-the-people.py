d0b11220e6562fd8a9768f87f31939acbe5a231f
Habib Gemmechu <95415986+habib-gm@users.noreply.github.com>
2023-12-27 17:51:21 +0300

    Added solution for Sort the People

---
+++ easy problems/sort-the-people.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(len(names)-1, -1, -1):
+            for j in range(i-1, -1, -1):
+                if(heights[i] > heights[j]):
+                    heights[i], heights[j] = heights[j], heights[i]
+                    names[i], names[j] = names[j], names[i]
+        
+        return names
+
