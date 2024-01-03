f0ff6c98ad17d89e07855fb9fd32a8ba3484ecee
Habib Gemmechu <95415986+habib-gm@users.noreply.github.com>
2024-01-03 17:33:42 +0300

    Added solution for Move Zeroes

---
+++ Leetcode Problems/Medium Problems/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0,0
+
+        while(right < len(nums)):
+            if(nums[left] != 0):
+                left += 1
+                right = left
+            elif(nums[right] == 0):
+                right += 1
+            else:
+                nums[right], nums[left] = nums[left], nums[right]
+                
+        
