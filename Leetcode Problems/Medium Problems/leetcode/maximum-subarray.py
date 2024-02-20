894071ca5a14fb85ff79ff7cde8e68256cdd7127
Endale Bob <95415986+endale-bob@users.noreply.github.com>
2024-02-20 18:56:26 +0300

    Add solution for Maximum Subarray

---
+++ Leetcode Problems/Medium Problems/leetcode/maximum-subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        left, right = 0, 0
+        window_sum, max_sum = 0, float(-inf)
+
+        while(right< len(nums)):
+            window_sum += nums[right]
+            right += 1
+
+            max_sum = max(window_sum, max_sum)
+
+            while(window_sum < 0):
+                window_sum -= nums[left]
+                left += 1
+         
+        return max_sum
+
+
+
+
+        
+
