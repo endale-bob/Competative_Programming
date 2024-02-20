4d4fc7580a4a5b8f683ea0e27f6e362581baacd7
Endale Bob <95415986+endale-bob@users.noreply.github.com>
2024-02-20 19:09:04 +0300

    Add solution for Merge Two Sorted Lists

--- Leetcode Problems/Medium Problems/leetcode/merge-two-sorted-lists.py
+++ Leetcode Problems/Medium Problems/leetcode/merge-two-sorted-lists.py
@@ -4,26 +4,27 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        root = ListNode()
-        head = root
-        while(list1 != None or list2 != None):
-            if(list1 == None):
-                root.next = list2
-                break
-            elif(list2 == None):
-                root.next = list1
-                break
-            else:
-                if(list1.val > list2.val):
-                    temp = list2
-                    list2 = list2.next
-                else:
-                    temp = list1
-                    list1 = list1.next
+    def recSol(self, list1: Optional[ListNode], list2: Optional[ListNode], root: ListNode) -> list[ListNode]:
+        if(list1 == None):
+            root.next = list2
+            return
+        elif(list2 == None):
+            root.next = list1
+            return 
 
-                temp.next = None
-                root.next = temp
-                root = root.next
+        if(list1.val > list2.val):
+            root.next = list2
+            list2 = list2.next
+        else:
+            root.next = list1
+            list1 = list1.next
+        
+        root = root.next
+        return self.recSol(list1, list2, root)
+        
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode("dummy")
+        self.recSol(list1, list2, head)
+
         return head.next
             
