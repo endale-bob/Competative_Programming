1a063c607cce8118e71a590ba9946e78669baf54
Habib Gemmechu <95415986+habib-gm@users.noreply.github.com>
2024-04-30 19:42:59 +0300

    Add solution for Permutations

--- Leetcode Problems/Medium Problems/leetcode/permutations.py
+++ Leetcode Problems/Medium Problems/leetcode/permutations.py
@@ -1,18 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def backtracking(temp=set(), lst=[]):
-            if(len(temp) == len(nums)):
-                res.append(lst[:])
+
+        def backtracking( lst=[], mask = 0):
+
+            if(len(lst) == len(nums)):
+                res.append(lst)
                 return 
 
-            for num in nums:
-                if(num in temp): continue
-                lst.append(num)
-                temp.add(num)
-                backtracking(temp, lst)
-                lst.pop()
-                temp.remove(num)
+            for idx, num in enumerate(nums):
+                if(mask & (1 << idx)): 
+                    continue
+
+                backtracking(lst + [num], mask | (1 << idx) )
 
         
         backtracking()
